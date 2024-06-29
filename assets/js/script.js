459e6cc7cc84a3133e3696ee7a18262d4e34aa3f
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-06-29 14:05:17 -0400

    added module and calendar widget

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+    $('#date-input').datepicker();
+});
+
+$('#date-input').datepicker();
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -18,12 +24,12 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
